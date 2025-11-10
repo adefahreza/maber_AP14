c65ab8373d2a7f8e714a23a18310bb57facf48a4
Aqilah Raihana <aqilahraihana9@gmail.com>
2025-11-10 17:36:36 +0800

    Update fitur reward

--- reward/reward.py
+++ reward/reward.py
@@ -3,19 +3,19 @@ from database.config import player_name
 
 def cek_tingkat(total_level):
     if 10 <= total_level < 20:
-        tingkat = "Tingkat 1 = Bronze"
+        tingkat = "Bronze"
     elif 20 <= total_level < 30:
-        tingkat = "Tingkat 2 = Silver"
+        tingkat = "Silver"
     elif 30 <= total_level < 40:
-        tingkat = "Tingkat 3 = Gold"
+        tingkat = "Gold"
     elif 40 <= total_level < 50:
-        tingkat = "Tingkat 4 = Diamond"
+        tingkat = "Diamond"
     elif 50 <= total_level < 60:
-        tingkat = "Tingkat 5 = Master"
+        tingkat = "Master"
     elif total_level == 60:
-        tingkat = "Tingkat 6 = Legendary"
+        tingkat = "Legendary"
     else:
-        tingkat = ""
+        tingkat = "Belum memiliki tingkatan"
     
     
     return tingkat
@@ -29,5 +29,7 @@ def proses_level(kategori, level_dikerjakan, total_level):
     
     if total_level % 10 == 0:
         print(f"🎉 Selamat! Anda naik ke {tingkat}")
+    elif 0 <= total_level >= 9:
+        print(f"Anda belum memiliki tingkatan")
     else:
-        print(f"Anda berada di {tingkat}.")
+        print(f"Anda berada di tingkat {tingkat}.")
